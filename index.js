3e80c1939f93dbed62ea72eb42dd71366f44dfed
Melanie Elson <melanie.elson@icloud.com>
2021-04-19 21:23:44 -0600

    add html and cards

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 // global variables 
 const inquirer = require('inquirer');
-
+const path = require('path');
 const fs = require('fs');
 const generateHTML = require("./src/generateHTML");
 const Manager = require("./lib/Manager");
 const Engineer = require("./lib/Engineer");
 const Employee = require("./lib/Employee");
 const Intern = require("./lib/Intern");
-
+const OUTPUT_DIR = path.resolve(__dirname, "output")
+const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const teamMembers = [];
 
---
+++ test/Employee.test.js
@@ -0,0 +1,31 @@
+const Employee  = require("../lib/Employee");
+
+describe("User selects new employee, () => {
+    const createEmployee = new Employee();
+    expect(typeof(createEmployee)).toBe("object");
+  });
+
+
+  describe("User puts in id", () => {
+    const createID = new ID();
+    expect(typeof(createID)).toBe("object");
+  });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
